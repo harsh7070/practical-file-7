ddae0908edefef0f252b354622e97bb02226a329
harsh7070 <110348525+harsh7070@users.noreply.github.com>
2022-10-13 01:35:33 +0530

    Add files via upload

---
+++ Practical_7/p7_1.java
@@ -0,0 +1,20 @@
+public class p7_1 {
+    public static <E> void printArray(E[] inputArray) {
+        for (E element : inputArray) {
+            System.out.printf("%s", element);
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Integer[] intArray = { 1, 2, 3, 4, 5 };
+        Double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+        Character[] charArray = {'H','E','L','L','O'};
+        System.out.println("Array integerArray contains:");
+        printArray(intArray);
+        System.out.println("Array doubleArray contains:");
+        printArray(doubleArray);
+        System.out.println("Array characterArray contains:");
+        printArray(charArray);
+        System.out.println("This Program is created by 21CE039_Harsh");
+    }
+}
